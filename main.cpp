3e4d6b65a05e375ddab521d8df38f246db367ef0
Agustín Ruiz <aeruiz@fi.uba.ar>
2024-10-03 23:18:51 +0000

    Message & press Ctrl Enter

--- main.cpp
+++ main.cpp
@@ -3,20 +3,25 @@
 
 int main()
 {
+    //Constructor
     DigitalIn gasDetector(D2);
-
+    //Constructor
     DigitalOut alarmLed(LED1);
-
+    
+    //Metodo
     gasDetector.mode(PullDown);
+
 
     alarmLed = OFF;
 
     while (true) {
         if ( gasDetector == ON ) {
+            printf("%s\n", "El detector de gas esta encendido.");
             alarmLed = ON;
         }
         
         if ( gasDetector == OFF ) {
+            printf("%s\n", "El detector de gas esta apagado.");
             alarmLed = OFF;
         }
     }
